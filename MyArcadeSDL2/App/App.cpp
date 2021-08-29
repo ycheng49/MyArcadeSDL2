ae2c1cae90d22676194bf8a306a4531f96d7ccd3
ycheng49 <yijie.cheng49@gmail.com>
2021-08-29 00:38:25 -0400

    Make a main loop of the app in the Run method in the class App

--- MyArcadeSDL2/App/App.cpp
+++ MyArcadeSDL2/App/App.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <SDL2/SDL.h>
+#include <iostream>
 
 #include "App.hpp"
 #include "Line2D.hpp"
@@ -32,24 +33,35 @@ void App::Run()
     if(mnoptrWindow)
     {
         Line2D line = {Vec2D(0,0), Vec2D(mScreen.Width(), mScreen.Height())};
-        mScreen.Draw(line, Color::White());
-        
         Triangle triangle = {Vec2D(60, 10), Vec2D(10, 110), Vec2D(110, 110)};
-        mScreen.Draw(triangle, Color::Red(), true, Color::Red());
-        
         AARectangle rect = {Vec2D(mScreen.Width() / 2 - 25, mScreen.Height() / 2 - 25), 50, 50};
-        mScreen.Draw(rect, Color::Blue(), true, Color::Blue());
-        
         Circle circle = {Vec2D(mScreen.Width() / 2 + 50, mScreen.Height() / 2 + 50), 50};
-        mScreen.Draw(circle, Color(0, 255, 0, 150), true, Color(0, 255, 0, 150));
-        
-        mScreen.SwapScreens();
-        
+                
         SDL_Event sdlEvent;
         bool running = true;
         
+        uint32_t lastTick = SDL_GetTicks();
+        uint32_t currentTick = lastTick;
+        
+        uint32_t dt = 10;       // in miliseconds
+        uint32_t accumulator = 0;
+        
         while(running)
         {
+            currentTick = SDL_GetTicks();
+            
+            uint32_t frameTime = currentTick - lastTick;
+            
+            if(frameTime > 300)         // in miliseconds
+            {
+                frameTime = 300;
+            }
+            
+            lastTick = currentTick;
+            
+            accumulator += frameTime;
+            
+            // input
             while(SDL_PollEvent(&sdlEvent))
             {
                 switch (sdlEvent.type) {
@@ -58,6 +70,23 @@ void App::Run()
                         break;
                 }
             }
+            
+            // update
+            while(accumulator >= dt)
+            {
+                // update current scene by dt
+                std::cout << "Delta time step: " << dt << std::endl;
+                
+                accumulator -= dt;
+            }
+            
+            // render
+            mScreen.Draw(line, Color::White());
+            mScreen.Draw(triangle, Color::Red(), true, Color::Red());
+            mScreen.Draw(rect, Color::Blue(), true, Color(0, 0, 255, 140));
+            mScreen.Draw(circle, Color(0, 255, 0, 150), true, Color(0, 255, 0, 150));
+            
+            mScreen.SwapScreens();
         }
     }
 }
